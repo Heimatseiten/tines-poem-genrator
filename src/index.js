1d06bdaa573647301da91e96b9dbd0f6929354da
Heimatseiten <159701446+Heimatseiten@users.noreply.github.com>
2024-03-31 15:56:58 +0200

    Added basic Form handling and added Typewriter Plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "1st row of poem",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
