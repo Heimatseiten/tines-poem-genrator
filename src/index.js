d6ec8a7d4f8f59c1de5a2a47aa4c854a20d2e7a7
Heimatseiten <159701446+Heimatseiten@users.noreply.github.com>
2024-03-31 17:18:49 +0200

    added axios, added further styling, added AI API imigration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "1st row of poem",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generatePoem(event) {
+  event.preventDefault();
+  let wordInput = document.querySelector("#word");
+
+  let apiKey = "9tb7143e5ff804f5od02a6f195ae433e";
+  let prompt = `Generate a romantic german poem that rhymes including the word ${wordInput.value}`;
+  let context =
+    "You are a romantic Poem expert and love to tell short and funny Love Poems. Your mission is to generate 4 line poems.Please use basic HTML.Please seperate the sentences with <br/>. Make sure to genrate the poem considering the input given in the form.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
